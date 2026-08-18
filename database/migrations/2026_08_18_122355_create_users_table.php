43c693a3131df1f6a1c7f1d1498b7b574db06f16
Brandon <brandonvel231@gmail.com>
2026-08-18 08:48:09 -0400

    initial database

---
+++ database/migrations/2026_08_18_122355_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); //Nombre del usuario
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('item')->unique(); //Item corporativo del usuario
+            $table->integer('id_card')->unique(); //Carnet de identidad del usuario
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2026_08_18_123953_create_user_leave_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_leave', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id') //llave foranea que hace referencia a la tabla users
+                  ->nullable(false)
+                  ->constrained()
+                  ->onDelete('cascade');
+            $table->foreignId('leave_id')  //llave foranea que hace referencia a la tabla leaves
+                  ->nullable(false)
+                  ->constrained()
+                  ->onDelete('cascade');
+            $table->timestamps();
+            $table->dateTime('exit_time')->nullable(false); //fecha y hora de salida
+            $table->dateTime('return_time')->nullable(true); //fecha y hora de retorno
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_leave');
+    }
+};
